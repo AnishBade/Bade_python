445194e02c86e427c54712e79b0198cee81bbc3e
anish <badeanish6045@gmail.com>
2020-05-06 00:09:46 +0545

    5/6

---
+++ COURSERA_PYTHON/Python_3_programming/Python_Basics/importing_modules-alternative.py
@@ -0,0 +1,5 @@
+from random import random,randrange
+#imports the particular modules from the library
+first=random() #returns any float number betwee 0-1
+print(first)
+print(randrange(1,7)) #returns any random int number in the range
---
+++ COURSERA_PYTHON/Python_3_programming/Python_Basics/importing_modules_1.py
@@ -0,0 +1,4 @@
+import random as rd
+first=rd.random() #returns any float number betwee 0-1
+print(first)
+print(rd.randrange(1,7)) #returns any random int number in the range
